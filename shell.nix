1a4cda07c6914f5d70b1ae88e1c1b6400d064984
Matthieu Coudron <mattator@gmail.com>
2019-04-15 21:06:04 +0900

    added shell.nix

---
+++ shell.nix
@@ -0,0 +1,45 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? nixpkgs.haskell.packages.ghc864 }:
+
+with nixpkgs;
+
+let
+  # ce sont ces dependences 
+  my_nvim = genNeovim [ haskellPackages.netlink ] nvimConfig;
+
+
+  nvimConfig = {
+    withHaskell = true;
+  };
+
+  my_netlink = haskell.lib.addBuildDepends compiler.netlink [ compiler.fast-logger ];
+in
+# haskellPackages.shellFor {
+compiler.shellFor {
+  # the dependencies of packages listed in `packages`, not the
+  packages = p: with p; [
+    # (import ./. { inherit compiler;})
+    my_netlink
+  ];
+  withHoogle = true;
+  # haskellPackages.stack
+  nativeBuildInputs = with compiler; [
+    hie
+    cabal-install
+    # haskellPackages.bytestring-conversion
+    gutenhasktags
+    haskdogs # seems to build on hasktags/ recursively import things
+    hasktags
+    # haskellPackages.nvim-hs
+    # haskellPackages.nvim-hs-ghcid
+
+    # for https://hackage.haskell.org/package/bytestring-conversion-0.2/candidate/docs/Data-ByteString-Conversion-From.html
+  ];
+
+  # export HIE_HOOGLE_DATABASE=$NIX_GHC_DOCDIR as DOCDIR doesn't exist it won't work
+  shellHook = ''
+    # check if it's still needed ?
+    export HIE_HOOGLE_DATABASE="$NIX_GHC_LIBDIR/../../share/doc/hoogle/index.html"
+    echo ${my_nvim}
+  '';
+}
+
